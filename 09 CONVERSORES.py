bb91221e4e12287deae604e73da937ceede25ab6
karolcampos9 <110216897+karolcampos9@users.noreply.github.com>
2023-03-27 20:39:09 -0600

    Add files via upload

---
+++ 09 CONVERSORES.py
@@ -0,0 +1,452 @@
+## SISTEMA DE CONVERSIONES IMPROVISADO
+## Inspirado en Pexi y en BlondFerretRabbit System - [aZafireCatWhoWasHere]
+## BY: aZafireCatWhoWasHere
+
+print("Sistema de conversión: Opaleye")
+print("Creado y programado por: aZafireCatWhoWasHere")
+
+resultado = 0
+print ("""
+    1) Monedas
+    2) Longitud
+    3) Masa
+    4) Almacenamiento
+    5) Tiempo
+    6) Volumen
+    7) Área
+    8) Manzanas
+    9) Hectáreas
+    ...
+    """)
+opcion = int(input("Seleccione una opción: ") )
+
+    #CONVERSOR DE MONEDAS = COMPLETO 2023/2/21 
+if opcion == 1:
+    print("Conversor de monedas")
+    print("""
+            Opciones:
+                1) Euros a Dolares
+                2) Dolares a Euros
+                3) Dolares a Yenes
+                4) Yenes a Dolares
+                5) Quetzales a Dolares
+                6) Dolares a Quetzales
+            """)
+    moneda = int(input("Ingrese la conversión de moneda que necesita: "))
+    
+    if moneda == 1:
+        print("Euros a Dolares")
+        entrada = float(input("Ingresa la cantidad en Euros: "))
+        resultado = round(entrada*1.07,2)
+    elif moneda == 2:
+        print("Dolares a Euros")
+        entrada = float(input("Ingresa la cantidad en Dolares: "))
+        resultado = round(entrada*0.94,2)
+    elif moneda == 3:
+        print("Dolares a Yenes")
+        entrada = float(input("Ingresa la cantidad en Dolares: "))
+        resultado = round(entrada*134.78,2)
+    elif moneda == 4:
+        print("Yenes a Dolares")
+        entrada = float(input("Ingresa la cantidad en Yenes: "))
+        resultado = round(entrada*0.0074,2)
+    elif moneda == 5:
+        print("Quetzales a Dolares")
+        entrada = float(input("Ingresa la cantidad en Quetzales: "))
+        resultado = round(entrada*0.13,2)
+    elif moneda == 6:
+        print("Dolares a Quetzales")
+        entrada = float(input("Ingresa la cantidad en Dolares: "))
+        resultado = round(entrada*7.82,2)
+    else:
+        print("Parece que te has equivocado en algo")
+        resultado = int(input("""
+            Me temo que esta moneda no esta registrada en el sistema de
+            conversión de Opaleye. ¡Por favor intentar de nuevo!
+            """))
+    
+
+    #CONVERSOR DE LONGITUDES = COMPLETADO 2023/02/28 
+elif opcion == 2:
+    print("Conversor de longitudes")
+    print("""
+            Opciones:
+                1) Milímetro a Centímetro
+                2) Centímetro a Milímetro
+                3) Metro a Centímetro
+                4) Centímetro a Metro
+                5) Metro a Kilómetro
+                6) Kilómetro a Metro
+            """)
+    longitud = int(input("Ingrese la conversión de unidad que necesita: "))
+    if longitud == 1:
+        print("Milímetro a Centímetro")
+        entrada = float(input("Ingresa la cantidad en milímetros: "))
+        resultado = round(entrada*0.1,2)
+    elif longitud == 2:
+        print("Centímetro a Milímetro")
+        entrada = float(input("Ingresa la cantidad en centímetros: "))
+        resultado = round(entrada*10)
+    elif longitud == 3:
+        print("Metro a Centímetro")
+        entrada = float(input("Ingresa la cantidad en metros: "))
+        resultado = round(entrada*100)
+    elif longitud == 4:
+        print("Centímetro a Metro")
+        entrada = float(input("Ingresa la cantidad en centímetros: "))
+        resultado = round(entrada*0.01,2)
+    elif longitud == 5:
+        print("Metro a Kilómetro")
+        entrada = float(input("Ingresa la cantidad en metros: "))
+        resultado = round(entrada*0.001,3)
+    elif longitud == 6:
+        print("Kilómetro a Metro")
+        entrada = float(input("Ingresa la cantidad en kilómetros: "))
+        resultado = round(entrada*1000)
+    else:
+        print("Parece que te has equivocado en algo")
+        resultado = int(input("""
+            Me temo que esta unidad no esta registrada en el sistema de
+            conversión de Opaleye. ¡Por favor intentar de nuevo!
+            """))
+    
+
+
+    #CONVERSOR DE MASAS = TERMINADO 2023/02/28
+elif opcion == 3:
+    print("Conversor de masas")
+    print("""
+            Opciones:
+                1) Miligramo a Centigramo
+                2) Centigramo a Miligramo
+                3) Decigramo a Centigramo
+                4) Decigramo a Gramo
+                5) Gramo a Decagramo
+                6) Gramo a Kilogramo
+            """)
+    masas = int(input("Ingrese la conversión de masas que necesita: "))
+    if masas == 1:
+        print("Miligramo a Centigramo")
+        entrada = float(input("Ingresa la cantidad en miligramos: "))
+        resultado = round(entrada*0.1,2)
+    elif masas == 2:
+        print("Centigramo a Miligramo")
+        entrada = float(input("Ingresa la cantidad en centigramos: "))
+        resultado = round(entrada*10)
+    elif masas == 3:
+        print("Decigramo a Centigramo")
+        entrada = float(input("Ingresa la cantidad en decigramos: "))
+        resultado = round(entrada*10)
+    elif masas == 4:
+        print("Decigramo a Gramo")
+        entrada = float(input("Ingresa la cantidad en decigramos: "))
+        resultado = round(entrada*0.1,2)
+    elif masas == 5:
+        print("Gramo a Decagramo")
+        entrada = float(input("Ingresa la cantidad en gramos: "))
+        resultado = round(entrada*0.1,2)
+    elif masas == 6:
+        print("Gramo a Kilogramo")
+        entrada = float(input("Ingresa la cantidad en gramos: "))
+        resultado = round(entrada*0.001,2)
+    else:
+        print("Parece que te has equivocado en algo")
+        resultado = int(input("""
+            Me temo que este dato no esta registrada en el sistema de
+            conversión de Opaleye. ¡Por favor intentar de nuevo!
+            """))
+
+    #CONVERSOR DE ALMACENAMIENTO = COMPLETADO 2023/02/28
+elif opcion == 4:
+    print("Conversor de almacenamiento")
+    print("""
+            Opciones:
+                1) Kilobyte a Bytes
+                2) Bytes a Kilobyte
+                3) Megabyte a Gigabyte
+                4) Gigabyte a Megabyte
+                5) Terabyte a Gigabyte
+                6) Gigabyte a Terabyte
+            """)
+    almacenamiento = int(input("Ingrese la conversión de unidad que necesita: "))
+    if almacenamiento == 1:
+        print("Kilobyte a Bytes")
+        entrada = float(input("Ingresa la cantidad en kilobytes: "))
+        resultado = round(entrada*1000)
+    elif almacenamiento == 2:
+        print("Bytes a Kilobyte")
+        entrada = float(input("Ingresa la cantidad en bytes: "))
+        resultado = round(entrada*0.001,2)
+    elif almacenamiento == 3:
+        print("Megabyte a Gigabyte")
+        entrada = float(input("Ingresa la cantidad en megabytes: "))
+        resultado = round(entrada*0.00097656,3)
+    elif almacenamiento == 4:
+        print("Gigabyte a Megabyte")
+        entrada = float(input("Ingresa la cantidad en gigabytes: "))
+        resultado = round(entrada*1000)
+    elif almacenamiento == 5:
+        print("Terabyte a Gigabyte")
+        entrada = float(input("Ingresa la cantidad en terabytes: "))
+        resultado = round(entrada*1024)
+    elif almacenamiento == 6:
+        print("Gigabyte a Terabyte")
+        entrada = float(input("Ingresa la cantidad en gigabytes: "))
+        resultado = round(entrada*0.00097656,3)
+    else:
+        print("Parece que te has equivocado en algo")
+        resultado = int(input("""
+            Me temo que este dato no esta registrada en el sistema de
+            conversión de Opaleye. ¡Por favor intentar de nuevo!
+            """))
+    
+
+    #CONVERSOR DE TIEMPO = COMPLETADO 2023/02/21
+elif opcion == 5:
+    print("Conversor de tiempo")
+    print("""
+            Opciones:
+                1) Segundos a Minutos
+                2) Minutos a Segundos
+                3) Horas a Minutos
+                4) Minutos a Horas
+                5) Horas a Días
+                6) Días a Horas
+            """)
+    tiempo = int(input("Ingrese la conversión de tiempo que necesita: "))
+
+    if tiempo == 1:
+        print("Segundos a Minutos")
+        entrada = float(input("Ingresa la cantidad en segundos: "))
+        resultado = round(entrada*0.0166667,3)
+    elif tiempo == 2:
+        print("Minutos a Segundos")
+        entrada = float(input("Ingresa la cantidad en minutos: "))
+        resultado = round(entrada*60)
+    elif tiempo == 3:
+        print("Horas a minutos")
+        entrada = float(input("Ingresa la cantidad en horas: "))
+        resultado = round(entrada*60)
+    elif tiempo == 4:
+        print("Minutos a Horas")
+        entrada = float(input("Ingresa la cantidad en minutos: "))
+        resultado = round(entrada*0.0166667,2)
+    elif tiempo == 5:
+        print("Horas a Días")
+        entrada = float(input("Ingresa la cantidad en horas: "))
+        resultado = round(entrada*0.0416667,2)
+    elif tiempo == 6:
+        print("Días a Horas")
+        entrada = float(input("Ingresa la cantidad en días: "))
+        resultado = round(entrada*24)
+    else:
+        print("Parece que te has equivocado en algo")
+        resultado = int(input("""
+            Me temo que este dato no esta registrado en el sistema de
+            conversión de Opaleye. ¡Por favor intentar de nuevo!
+            """))
+
+
+    #CONVERSOR DE VOLUMEN = COMPLETADO 2023/03/07
+elif opcion == 6:
+    print("Conversor de volumen")
+    print("""
+            Opciones:
+                1) Milímetro cúbico a Centímetro cúbico
+                2) Centímetro cúbico a Milímetro cúbico
+                3) Metro cúbico a Centímetro cúbico
+                4) Centímetro cúbico a Metro cúbico
+                5) Metro cúbico a Kilómetro cúbico
+                6) Kilómetro cúbico a Metro cúbico
+            """)
+    volumen = int(input("Ingrese la conversión de volumen que necesita: "))
+    
+    if volumen == 1:
+        print("Milímetro cúbico a Centímetro cúbico")
+        entrada = float(input("Ingresa la cantidad en milímetros cúbicos: "))
+        resultado = round(entrada*0.001,4)
+        
+    elif volumen == 2:
+        print("Centímetro cúbico a Milímetro cúbico")
+        entrada = float(input("Ingresa la cantidad en centímetros cúbicos: "))
+        resultado = round(entrada*1000)
+        
+    elif volumen == 3:
+        print("Metro cúbico a Centímetro cúbico")
+        entrada = float(input("Ingresa la cantidad en metros cúbicos: "))
+        resultado = round(entrada*1000000)
+        
+    elif volumen == 4:
+        print("Centímetro cúbico a Metro cúbico")
+        entrada = float(input("Ingresa la cantidad en centímetros cúbicos: "))
+        resultado = round(entrada*0.000001,6)
+        
+    elif volumen == 5:
+        print("Metro cúbico a Kilómetro cúbico")
+        entrada = float(input("Ingresa la cantidad en metros cúbicos: "))
+        resultado = round(entrada*0.000000000001,12)
+        
+    elif volumen == 6:
+        print("Kilómetro cúbico a Metro cúbico")
+        entrada = float(input("Ingresa la cantidad en kilómetros cúbicos: "))
+        resultado = round(entrada*1000000000)
+        
+    else:
+        print("Parece que te has equivocado en algo")
+        resultado = int(input("""
+            Me temo que esta capacidad no esta registrada en el sistema de
+            conversión de Opaleye. ¡Por favor intentar de nuevo!
+            """))
+        
+    print("El resultado de la conversión es:", resultado)
+
+
+    #CONVERSOR DE AREA = COMPLETADO 2023/03/05
+
+if opcion == 7:
+    print("Conversor de áreas")
+    print("""
+            Opciones:
+                1) Pies cuadrados a Metros cuadrados
+                2) Metros cuadrados a Pies cuadrados
+                3) Acres a Hectáreas
+                4) Hectáreas a Acres
+                5) Millas cuadradas a Kilómetros cuadrados
+                6) Kilómetros cuadrados a Millas cuadradas
+            """)
+    area = int(input("Ingrese la conversión de área que necesita: "))
+    
+    if area == 1:
+        print("Pies cuadrados a Metros cuadrados")
+        entrada = float(input("Ingresa la cantidad en Pies cuadrados: "))
+        resultado = round(entrada*0.092903,2)
+    elif area == 2:
+        print("Metros cuadrados a Pies cuadrados")
+        entrada = float(input("Ingresa la cantidad en Metros cuadrados: "))
+        resultado = round(entrada*10.764,2)
+    elif area == 3:
+        print("Acres a Hectáreas")
+        entrada = float(input("Ingresa la cantidad en Acres: "))
+        resultado = round(entrada*0.404686,2)
+    elif area == 4:
+        print("Hectáreas a Acres")
+        entrada = float(input("Ingresa la cantidad en Hectáreas: "))
+        resultado = round(entrada*2.47105,2)
+    elif area == 5:
+        print("Millas cuadradas a Kilómetros cuadrados")
+        entrada = float(input("Ingresa la cantidad en Millas cuadradas: "))
+        resultado = round(entrada*2.58999,2)
+    elif area == 6:
+        print("Kilómetros cuadrados a Millas cuadradas")
+        entrada = float(input("Ingresa la cantidad en Kilómetros cuadrados: "))
+        resultado = round(entrada*0.386102,2)
+    else:
+        print("Parece que te has equivocado en algo")
+        resultado = int(input("""
+            Me temo que esta conversión de área no esta registrada en el sistema
+            de conversión de Opaleye. ¡Por favor intentar de nuevo!
+            """))
+    print("El resultado de la conversión es:", resultado)
+
+    #CONVERSOR DE MANZANAS = COMPLETADO 2023/03/13
+if opcion == 8:
+    print("Conversor de Manzanas")
+    print("""
+            Opciones:
+                1) Varas cuadradas a Manzanas
+                2) Manzanas a Metros cuadrados
+                3) Manzanas a Hectáreas
+                4) Acres a Manzanas
+                5) Hectáreas a Manzanas
+            """)
+    area = int(input("Ingrese la conversión de área que necesita: "))
+    
+    if area == 1:
+        print("Varas cuadradas a Manzanas")
+        entrada = float(input("Ingresa la cantidad en Varas cuadradas: "))
+        resultado = round(entrada/640,2)
+    elif area == 2:
+        print("Manzanas a Metros cuadrados")
+        entrada = float(input("Ingresa la cantidad en Manzanas: "))
+        resultado = round(entrada*6988.98,2)
+    elif area == 3:
+        print("Manzanas a Hectáreas")
+        entrada = float(input("Ingresa la cantidad en Manzanas: "))
+        resultado = round(entrada*0.7,2)
+    elif area == 4:
+        print("Acres a Manzanas")
+        entrada = float(input("Ingresa la cantidad en Acres: "))
+        resultado = round(entrada*1.6,2)
+    elif area == 5:
+        print("Hectáreas a Manzanas")
+        entrada = float(input("Ingresa la cantidad en Hectáreas: "))
+        resultado = round(entrada/1.43,2)
+    else:
+        print("Parece que te has equivocado en algo")
+        resultado = int(input("""
+            Me temo que esta conversión de área no está registrada en el sistema
+            de conversión de Opaleye. ¡Por favor intentar de nuevo!
+            """))
+    print("El resultado de la conversión es:", resultado)
+
+
+    #CONVERSOR DE HECTAREAS = COMPLETADO 2023/03/13
+if opcion == 9:
+    print("Conversor de Hectáreas")
+    print("""
+            Opciones:
+                1) Hectáreas a Milímetros cuadrados
+                2) Hectáreas a Centímetros cuadrados
+                3) Hectáreas a Decímetros cuadrados
+                4) Hectáreas a Metros cuadrados
+                5) Áreas a Hectáreas
+                6) Kilómetros cuadrados a Hectáreas
+            """)
+    area = int(input("Ingrese la conversión de área que necesita: "))
+    
+    if area == 1:
+        print("Hectáreas a Milímetros cuadrados")
+        entrada = float(input("Ingresa la cantidad en Hectáreas: "))
+        resultado = round(entrada*10000000000,2)
+    elif area == 2:
+        print("Hectáreas a Centímetros cuadrados")
+        entrada = float(input("Ingresa la cantidad en Hectáreas: "))
+        resultado = round(entrada*1000000,2)
+    elif area == 3:
+        print("Hectáreas a Decímetros cuadrados")
+        entrada = float(input("Ingresa la cantidad en Hectáreas: "))
+        resultado = round(entrada*10000,2)
+    elif area == 4:
+        print("Hectáreas a Metros cuadrados")
+        entrada = float(input("Ingresa la cantidad en Hectáreas: "))
+        resultado = round(entrada*10000,2)
+    elif area == 5:
+        print("Áreas a Hectáreas")
+        entrada = float(input("Ingresa la cantidad en Áreas: "))
+        resultado = round(entrada*0.01,2)
+    elif area == 6:
+        print("Kilómetros cuadrados a Hectáreas")
+        entrada = float(input("Ingresa la cantidad en Kilómetros cuadrados: "))
+        resultado = round(entrada*100,2)
+    else:
+        print("Parece que te has equivocado en algo")
+        resultado = int(input("""
+            Me temo que esta conversión de área no está registrada en el sistema
+            de conversión de Opaleye. ¡Por favor intentar de nuevo!
+            """))
+    print("El resultado de la conversión es:", resultado)
+
+
+    #NÚMERO PARA SISTEMA DE CONVERSIÓN INEXISTENTE
+else:
+    print("Parece que te has equivocado en algo")
+    resultado = int(input("""
+        No hay conversión, revisar que se haya colocado un número
+        en la selección del sistema de conversión y no el sistema
+        """))
+
+#RESULTADO
+print("El resultado de la conversión es: {} ".format(resultado))
+#AGRADECIMIENTOS
+print("¡Gracias por usar el sistema Opaleye!")
+print("@aZafireCatWhoWasHere espera haber ayudado")
